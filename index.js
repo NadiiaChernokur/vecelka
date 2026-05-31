de1bd066598e53debf69ccf155f8bc08ab6bdd41
NadiiaChernokur <chiernokur5@gmail.com>
2026-06-01 00:41:40 +0200

    button

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+import { themes } from "./temes.js";
+
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-/* ---------------- THEMES ---------------- */
+// /* ---------------- THEMES ---------------- */
 
-const themes = [
-  { bg: "#1e3a8a" },
-  { bg: "#065f46" },
-  { bg: "#7c2d12" },
-  { bg: "#4c1d95" },
-  { bg: "#0f172a" },
-  { bg: "#831843" },
-  { bg: "#164e63" },
-  { bg: "#3f3f46" },
-  { bg: "#b91c1c" },
-  { bg: "#92400e" },
-];
+//   const themes = [
+//     { bg: "#1e3a8a" },
+//     { bg: "#065f46" },
+//     { bg: "#7c2d12" },
+//     { bg: "#4c1d95" },
+//     { bg: "#0f172a" },
+//     { bg: "#831843" },
+//     { bg: "#164e63" },
+//     { bg: "#3f3f46" },
+//     { bg: "#b91c1c" },
+//     { bg: "#92400e" },
+//   ];
 
 let currentThemeIndex = 0;
 let theme = themes[currentThemeIndex];
@@ -82,7 +84,10 @@ animate();
 
 /* ---------------- THEME BUTTON ---------------- */
 
-document.querySelector(".button").addEventListener("click", () => {
+const themeButton = document.querySelector(".button");
+themeButton.style.backgroundColor = theme.bg;
+
+themeButton.addEventListener("click", () => {
   currentThemeIndex++;
 
   if (currentThemeIndex >= themes.length) {
@@ -90,4 +95,6 @@ document.querySelector(".button").addEventListener("click", () => {
   }
 
   theme = themes[currentThemeIndex];
+
+  themeButton.style.backgroundColor = theme.bg;
 });
